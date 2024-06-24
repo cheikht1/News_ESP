d460b2b29f918c5d9995b2cff9ef287f4af9e298
cheikht1 <cheikhmouhamedtidianethiam@esp.sn>
2024-06-24 18:04:18 +0200

    modified:   db.php

--- db.php
+++ db.php
@@ -1,14 +1,28 @@
 <?php
-$servername = "localhost";
-$username = "cheikht";
-$password = "root";
-$dbname = "news";
+// $servername = "db"; // "localhost";
+// $username = "cheikht";
+// $password = "root";
+// $dbname = "news";
+
+// try {
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//     // Set the PDO error mode to exception
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch(PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
+// }
+
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Set the PDO error mode to exception
+    $conn = new PDO('mysql:host=db;dbname=news', 'cheikht', 'root');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
+
+
+
+
+
+
 ?>
